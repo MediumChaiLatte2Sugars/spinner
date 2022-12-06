8337b9959f7778222907945951fbf9ebc33be568
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-06 17:17:20 -0500

    Refactored to DRYer code

--- spinner2.js
+++ spinner2.js
@@ -2,15 +2,10 @@ const animationArray = ['\r|', '\r/', '\r-', '\r\\', '\r|'];
 let timer = 100;
 
 for (let iteration = 0; iteration < 2; iteration++) {
-  for (let animation of animationArray) {
+  for (let animation = 0; animation < animationArray.length; animation++) {
     setTimeout(() => {
-      process.stdout.write(animation + '  ');
+      animation === animationArray.length - 1 && iteration === 1 ? process.stdout.write(animationArray[animation] + '\n') : process.stdout.write(animationArray[animation] + '  ');
     }, timer);
-
     timer += 200;
   }
 }
-
-setTimeout(() => {
-  process.stdout.write('\r');
-}, timer + 50);
