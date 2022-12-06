2a3ac3a10968c1dc30b559bf33f3c57e79f98226
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-06 17:05:00 -0500

    Created spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,16 @@
+const animationArray = ['\r|', '\r/', '\r-', '\r\\', '\r|'];
+let timer = 100;
+
+for (let iteration = 0; iteration < 2; iteration++) {
+  for (let animation of animationArray) {
+    setTimeout(() => {
+      process.stdout.write(animation + '  ');
+    }, timer);
+
+    timer += 200;
+  }
+}
+
+setTimeout(() => {
+  process.stdout.write('\r');
+}, timer + 50);
